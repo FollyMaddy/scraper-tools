bcc04911f4864794f51690d8de91edd7a28c3140
FollyMaddy <meulenf@gmx.net>
2019-06-28 16:55:58 +0200

    Add files via upload
    removed some double info lines

--- msxmagazinescraper.sh
+++ msxmagazinescraper.sh
@@ -22,9 +22,6 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-# Make the program executable, dubbleclick and choose open in terminal.
-# Or run it from the terminal with : ./msxmagazinescraper.sh
-
 websitearchief=www.msxcomputermagazine.nl/archief
 maplist='
 bladen/ 
