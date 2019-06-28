78286cc76b6223e2b32901bc34fd10551d768aa3
FollyMaddy <meulenf@gmx.net>
2019-06-28 16:43:06 +0200

    Add files via upload
    rewritten to version 1.3

--- msxmagazinescraper.sh
+++ msxmagazinescraper.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
-# Date   : 27/06/2019
+# Program : msxmagazinescraper.sh
+# Version : 1.3
+# Use : 
+# Get the whole archive from www.msxcomputermagazine.nl/archief without a mouse-click
+# How to run :
+# Make the program executable, dubbleclick and choose open in terminal.
+# Or run it from the terminal with : ./msxmagazinescraper.sh
+# Author : Folkert van der Meulen
+# Date   : 28/06/2019 (rewritten,reprogrammed)
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -17,33 +25,86 @@
 # Make the program executable, dubbleclick and choose open in terminal.
 # Or run it from the terminal with : ./msxmagazinescraper.sh
 
-bladen=https://www.msxcomputermagazine.nl/archief/bladen/
-disks_art_gallery=https://www.msxcomputermagazine.nl/archief/disks/art-gallery/
-disks_extra=https://www.msxcomputermagazine.nl/archief/disks/extra/
-disks_lb=https://www.msxcomputermagazine.nl/archief/disks/lb/
-disks_mccm=https://www.msxcomputermagazine.nl/archief/disks/mccm/
-disks_mcm=https://www.msxcomputermagazine.nl/archief/disks/mcm/
-diskzips=https://www.msxcomputermagazine.nl/archief/diskzips/
-lb=https://www.msxcomputermagazine.nl/archief/lb/
-
-# Listings are not implemented !!! 
-# Too much work for this quick and dirty program.
-# For that i have to make a different program.
+websitearchief=www.msxcomputermagazine.nl/archief
+maplist='
+bladen/ 
+disks/art-gallery/ 
+disks/extra/ 
+disks/lb/ 
+disks/mccm/ 
+disks/mcm/ 
+diskzips/ 
+lb/ 
+listings/mcmd02.di1/
+listings/mcmd03.di1/
+listings/mcmd04.di1/
+listings/mcmd05.di1/
+listings/mcmd06.di1/
+listings/mcmd07.di1/
+listings/mcmd08.di1/
+listings/mcmd09.di1/
+listings/mcmd10.di1/
+listings/mcmd11.di1/
+listings/mcmd12.di1/
+listings/mcmd13.di1/
+listings/mcmd14.di1/
+listings/mcmd15.di1/
+listings/mcmd16.di1/
+listings/mcmd17.di1/
+listings/mcmd18.di1/
+listings/mcmd19.di1/
+listings/mcmd20.di1/
+listings/mcmd21.di1/
+listings/mcmd22.di1/
+listings/mcmd23.di1/
+listings/mcmd24.di1/
+listings/mcmd25.di1/
+listings/mcmd26.di1/
+listings/mcmd27.di1/
+listings/mcmd28.di1/
+listings/mcmd29.di1/
+listings/mcmd30.di1/
+listings/mcmd31.di1/
+listings/mcmd32.di1/
+listings/mcmd33.di1/
+listings/mcmd34.di1/
+listings/mcmd35.di1/
+listings/mcmd36.di1/
+listings/mcmd37.di1/
+listings/mcmd38.di1/
+listings/mcmd39.di1/
+listings/mcmd40.di1/
+listings/mcmd41.di1/
+listings/mcmd42.di1/
+listings/mcmd43.di1/
+listings/mcmd44.di1/
+listings/mcmd45.di1/
+listings/mcmd46.di1/
+listings/mcmd47.di1/
+listings/mcmd48.di1/
+listings/mcmd49.di1/
+listings/mcmd50.di1/
+listings/mcmd51.di1/
+listings/mcmd52.di1/
+listings/mcmd53.di1/
+listings/mcmd55.di2/
+listings/mcmd56.di1/'
 
 startworkdir=$(pwd)
 
-mkdir "$startworkdir/bladen"
-mkdir "$startworkdir/disks"
-mkdir "$startworkdir/disks/art-gallery"
-mkdir "$startworkdir/disks/extra"
-mkdir "$startworkdir/disks/lb"
-mkdir "$startworkdir/disks/mccm"
-mkdir "$startworkdir/disks/mcm"
-mkdir "$startworkdir/diskzips"
-mkdir "$startworkdir/lb"
+for map in $maplist
+do
+echo "create directory\'s $startworkdir/$websitearchief/$map"
+mkdir -p "$startworkdir/$websitearchief/$map"
+done
 
-cd "$startworkdir/bladen"
-curl -g -s -o "-" "$bladen" | while read LINE
+for map in $maplist
+do
+
+cd "$startworkdir/$websitearchief/$map"
+echo "Write files in map $startworkdir/$websitearchief/$map"
+curl -g -s -o "-" "https://$websitearchief/$map" | while read LINE
+
 do
 case "$LINE" in
   *li*)
@@ -51,129 +112,14 @@ case "$LINE" in
   # do stuff if not conains a / (you can add more options by add this to line (ignore \ also) -->) || [[ "$MAPORFILE" != *\\* ]]
   if [[ "$MAPORFILE" != *\/* ]] 
   then
-  echo curl -g -O $bladen$MAPORFILE
-  curl -g -O $bladen$MAPORFILE
+  echo curl -g -O https://$websitearchief/$map$MAPORFILE
+  curl -g -O https://$websitearchief/$map$MAPORFILE
   #echo $MAPORFILE
   fi 
 esac
 done
+
+done
+
 cd "$startworkdir"
 
-cd "$startworkdir/disks/art-gallery"
-curl -g -s -o "-" "$disks_art_gallery" | while read LINE 
-do
-case "$LINE" in
-  *li*)
-  MAPORFILE=$(echo $LINE| cut -d'"' -f 2)
-  # do stuff if not conains a / (you can add more options by add this to line (ignore \ also) -->) || [[ "$MAPORFILE" != *\\* ]]
-  if [[ "$MAPORFILE" != *\/* ]] 
-  then
-  echo curl -g -O $disks_art_gallery$MAPORFILE
-  curl -g -O $disks_art_gallery$MAPORFILE
-  #echo $MAPORFILE
-  fi  
-esac
-done
-cd "$startworkdir"
-
-cd "$startworkdir/disks/extra"
-curl -g -s -o "-" "$disks_extra" | while read LINE 
-do
-case "$LINE" in
-  *li*)
-  MAPORFILE=$(echo $LINE| cut -d'"' -f 2)
-  # do stuff if not conains a / (you can add more options by add this to line (ignore \ also) -->) || [[ "$MAPORFILE" != *\\* ]]
-  if [[ "$MAPORFILE" != *\/* ]] 
-  then
-  echo curl -g -O $disks_extra$MAPORFILE
-  curl -g -O $disks_extra$MAPORFILE
-  #echo $MAPORFILE
-  fi  
-esac
-done
-cd "$startworkdir"
-
-cd $startworkdir/disks/lb
-curl -g -s -o "-" "$disks_lb" | while read LINE 
-do
-case "$LINE" in
-  *li*)
-  MAPORFILE=$(echo $LINE| cut -d'"' -f 2)
-  # do stuff if not conains a / (you can add more options by add this to line (ignore \ also) -->) || [[ "$MAPORFILE" != *\\* ]]
-  if [[ "$MAPORFILE" != *\/* ]] 
-  then
-  echo curl -g -O $disks_lb$MAPORFILE
-  curl -g -O $disks_lb$MAPORFILE
-  #echo $MAPORFILE
-  fi  
-esac
-done
-cd "$startworkdir"
-
-cd "$startworkdir/disks/mccm"
-curl -g -s -o "-" "$disks_mccm" | while read LINE 
-do
-case "$LINE" in
-  *li*)
-  MAPORFILE=$(echo $LINE| cut -d'"' -f 2)
-  # do stuff if not conains a / (you can add more options by add this to line (ignore \ also) -->) || [[ "$MAPORFILE" != *\\* ]]
-  if [[ "$MAPORFILE" != *\/* ]] 
-  then
-  echo curl -g -O $disks_mccm$MAPORFILE
-  curl -g -O $disks_mccm$MAPORFILE
-  #echo $MAPORFILE
-  fi  
-esac
-done
-cd "$startworkdir"
-
-cd "$startworkdir/disks/mcm"
-curl -g -s -o "-" "$disks_mccm" | while read LINE 
-do
-case "$LINE" in
-  *li*)
-  MAPORFILE=$(echo $LINE| cut -d'"' -f 2)
-  # do stuff if not conains a / (you can add more options by add this to line (ignore \ also) -->) || [[ "$MAPORFILE" != *\\* ]]
-  if [[ "$MAPORFILE" != *\/* ]] 
-  then
-  echo curl -g -O $disks_mccm$MAPORFILE
-  curl -g -O $disks_mccm$MAPORFILE
-  #echo $MAPORFILE
-  fi  
-esac
-done
-cd "$startworkdir"
-
-cd "$startworkdir/diskzips"
-curl -g -s -o "-" "$diskzips" | while read LINE 
-do
-case "$LINE" in
-  *li*)
-  MAPORFILE=$(echo $LINE| cut -d'"' -f 2)
-  # do stuff if not conains a / (you can add more options by add this to line (ignore \ also) -->) || [[ "$MAPORFILE" != *\\* ]]
-  if [[ "$MAPORFILE" != *\/* ]] 
-  then
-  echo curl -g -O $diskzips$MAPORFILE
-  curl -g -O $diskzips$MAPORFILE
-  #echo $MAPORFILE
-  fi  
-esac
-done
-cd "$startworkdir"
-
-cd "$startworkdir/lb"
-curl -g -s -o "-" "$lb" | while read LINE
-do
-case "$LINE" in
-  *li*)
-  MAPORFILE=$(echo $LINE| cut -d'"' -f 2)
-  # do stuff if not conains a / (you can add more options by add this to line (ignore \ also) -->) || [[ "$MAPORFILE" != *\\* ]]
-  if [[ "$MAPORFILE" != *\/* ]] 
-  then
-  echo curl -g -O $lb$MAPORFILE
-  curl -g -O $lb$MAPORFILE
-  #echo $MAPORFILE
-  fi 
-esac
-done
-cd "$startworkdir"
